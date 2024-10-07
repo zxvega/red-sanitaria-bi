43ccb0b602d80101b5985cbd340121e3bd127125
zxvega <zxvega@gmail.com>
2024-10-07 16:34:04 -0400

    Añade Merge para el Hecho Atencion

---
+++ RedSanitariaDW/Security/Stored Procedures/DW_MergeFactAtencion.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeFactAtencion]
+AS
+BEGIN
+
+	UPDATE da
+	SET FechaIngresoKey    = sa.FechaIngresoKey
+	   ,FechaAltaKey = sa.FechaAltaKey
+	   ,HospitalKey  = sa.HospitalKey
+       ,ServicioKey      = sa.ServicioKey
+	   ,UnidadKey         = sa.UnidadKey
+	   ,MedicoKey         = sa.MedicoKey
+	   ,PacienteKey       = sa.PacienteKey
+	   ,Diagnostico        = sa.Diagnostico
+	   ,Tratamiento       = sa.Tratamiento
+	FROM [dbo].[FactAtencionMedica]         da
+	INNER JOIN [staging].[FactAtencionMedica] sa ON (da.AtencionSK = sa.AtencionSK )
+END
+GO
