0cb893688ba099d69b5238cb299f57d3537f18b2
zxvega <zxvega@gmail.com>
2024-10-09 14:55:15 -0400

    Actualizacion por cambios de equipo

--- RedSanitariaOLTP/Scripts/DataAtencion.sql
+++ RedSanitariaOLTP/Scripts/DataAtencion.sql
@@ -4,7 +4,7 @@ INSERT INTO [HIS].[Atencion]
 (Id, FechaIngreso, FechaAlta,   HoraIngreso, HoraAlta, HospitalId, ServicioId, UnidadId, MedicoId, PacienteId, AtencionOrigenId, TipoId, Diagnostico, Tratamiento)
 VALUES 
 (1, '2024-09-01', '2024-09-01', '08:30:00', '09:00:00', 1,			3,			3,			4,		100005,		NULL,	1,		'Síntomas de fiebre y malestar general. Se solicita un hemograma y una radiografía de tórax para evaluación', 'Reposo, hidratación y seguimiento de resultados de estudios.'), -- Consulta
-(2, '2024-09-02', '2024-09-02', '10:00:00', '10:30:00', 1,			1,			NULL,		NULL,	100005,		NULL,	2,		'Estudios de laboratorio realizados: hemograma y radiografía de tórax.', NULL), -- Examen de Gabinete
+(2, '2024-09-02', '2024-09-02', '10:00:00', '10:30:00', 1,			1,			NULL,		30,		100005,		NULL,	2,		'Estudios de laboratorio realizados: hemograma y radiografía de tórax.', NULL), -- Examen de Gabinete
 (3, '2024-09-03', '2024-09-03', '11:00:00', '11:30:00', 1,			3,			3,			4,		100005,		1,		1,		'Resultados indican neumonía leve. Se prescribe tratamiento con antibióticos y seguimiento', 'Antibióticos orales, control de síntomas y cita de seguimiento en una semana'); -- Reconsulta
 
 
