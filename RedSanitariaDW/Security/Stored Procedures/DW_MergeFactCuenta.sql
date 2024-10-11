757e885db2f3c0d6f5eee47b821db58eae9e3a0e
zxvega <zxvega@gmail.com>
2024-10-10 20:37:35 -0400

    Añade procedures para ETL de FactEstudiosRealizados - Detalle de Cuenta Paciente

---
+++ RedSanitariaDW/Security/Stored Procedures/DW_MergeFactCuenta.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeFactCuenta]
+AS
+BEGIN
+
+	UPDATE da
+	SET 
+	   FechaKey = sa.FechaKey
+	   ,HospitalKey  = sa.HospitalKey
+       ,ServicioKey      = sa.ServicioKey
+	   ,MedicoKey         = sa.MedicoKey
+	   ,PacienteKey       = sa.PacienteKey
+	   ,EstudioKey      = sa.EstudioKey
+	   ,Cantidad        = sa.Cantidad
+	   ,Precio       = sa.Precio
+	FROM [dbo].[FactEstudioRealizado]         da
+	INNER JOIN [staging].[FactEstudioRealizado] sa ON (da.EstudioSK = sa.EstudioSK )
+END
+GO
---
+++ RedSanitariaOLTP/Security/Stored Procedures/GetCuentaChangesByRowVersion.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].GetCuentaChangesByRowVersion
+(
+   @startRow BIGINT 
+   ,@endRow  BIGINT 
+)
+AS
+BEGIN
+	SELECT 
+		D.Id,
+		Fecha = CONVERT(INT,
+							(CONVERT(CHAR(4),DATEPART(YEAR,D.Fecha))
+						  + CASE 
+								WHEN DATEPART(MONTH,D.Fecha) < 10 THEN '0' + CONVERT(CHAR(1),DATEPART(MONTH,D.Fecha))
+								ELSE + CONVERT(CHAR(2),DATEPART(MONTH,D.Fecha))
+							END
+						  + CASE 
+								WHEN DATEPART(DAY,D.Fecha) < 10 THEN '0' + CONVERT(CHAR(1),DATEPART(DAY,D.Fecha))
+								ELSE + CONVERT(CHAR(2),DATEPART(DAY,D.Fecha))
+							END)),
+		A.HospitalId,
+		A.ServicioId,
+		A.MedicoId,
+		A.PacienteId,
+		D.EstudioId,
+		D.Cantidad,
+		D.Precio
+	FROM 
+		HIS.Atencion A 
+		INNER JOIN ERP.CuentaPaciente C ON A.Id = C.AtencionId 
+		INNER JOIN ERP.DetalleCuentaPaciente D ON C.Id = D.CuentaPacienteId
+	WHERE 
+		A.RowVersion > CONVERT(ROWVERSION,@startRow) 
+		AND A.RowVersion <= CONVERT(ROWVERSION,@endRow);
+
+END
+GO
